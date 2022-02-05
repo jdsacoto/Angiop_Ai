dd5f0206305e70cff1ad019d18cb0bcc80b43732
jdsacoto <46428518+jdsacoto@users.noreply.github.com>
2022-02-04 23:56:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,15 @@ if w:
 
     data = pd.read_csv(w)
     st.write(data)
+"""
+# File Downloader
+It's hard to test the ability to upload files in an automated way, so here you
+should test it by hand. Please upload a Zip file and make sure a table shows up
+below with its contents.
+"""    
+f = st.file_downloader("Upload a DCM.zip file", type="zip")
+if w:
+    import pandas as pd
+
+    data = pd.write_csv(f)
+    st.write(data)
