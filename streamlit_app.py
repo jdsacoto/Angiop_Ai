0e862cad2e8eaaf34da334dedcf596d357d9c713
jdsacoto <46428518+jdsacoto@users.noreply.github.com>
2022-02-04 23:58:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,36 @@ It's hard to test the ability to upload files in an automated way, so here you
 should test it by hand. Please upload a Zip file and make sure a table shows up
 below with its contents.
 """    
-f = st.file_downloader("Upload a DCM.zip file", type="zip")
-if w:
-    import pandas as pd
+# Text files
 
-    data = pd.write_csv(f)
-    st.write(data)
+text_contents = '''
+Foo, Bar
+123, 456
+789, 000
+'''
+
+# Different ways to use the API
+
+st.download_button('Download CSV', text_contents, 'text/csv')
+st.download_button('Download CSV', text_contents)  # Defaults to 'text/plain'
+
+with open('myfile.csv') as f:
+   st.download_button('Download CSV', f)  # Defaults to 'text/plain'
+
+# ---
+# Binary files
+
+binary_contents = b'whatever'
+
+# Different ways to use the API
+
+st.download_button('Download file', binary_contents)  # Defaults to 'application/octet-stream'
+
+with open('myfile.zip', 'rb') as f:
+   st.download_button('Download Zip', f, file_name='archive.zip')  # Defaults to 'application/octet-stream'
+
+# You can also grab the return value of the button,
+# just like with any other button.
+
+if st.download_button(...):
+   st.write('Thanks for downloading!')
