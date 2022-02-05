427fd64840fad54d2aff5e9547e2cc2a9ac48bc5
jdsacoto <46428518+jdsacoto@users.noreply.github.com>
2022-02-04 23:18:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,17 @@ import streamlit as st
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
+
+"""
+# File Uploader
+It's hard to test the ability to upload files in an automated way, so here you
+should test it by hand. Please upload a CSV file and make sure a table shows up
+below with its contents.
+"""
+
+w = st.file_uploader("Upload a CSV file", type="csv")
+if w:
+    import pandas as pd
+
+    data = pd.read_csv(w)
+    st.write(data)
