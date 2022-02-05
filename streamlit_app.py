52677b53a0b70eb94e2e439004399c5ca59ab813
jdsacoto <46428518+jdsacoto@users.noreply.github.com>
2022-02-04 23:59:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,18 +44,3 @@ with open('myfile.csv') as f:
 
 # ---
 # Binary files
-
-binary_contents = b'whatever'
-
-# Different ways to use the API
-
-st.download_button('Download file', binary_contents)  # Defaults to 'application/octet-stream'
-
-with open('myfile.zip', 'rb') as f:
-   st.download_button('Download Zip', f, file_name='archive.zip')  # Defaults to 'application/octet-stream'
-
-# You can also grab the return value of the button,
-# just like with any other button.
-
-if st.download_button(...):
-   st.write('Thanks for downloading!')
