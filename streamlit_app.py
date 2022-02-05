e9afe5cb960509bf7ea9ab2cb087cbb327879509
jdsacoto <46428518+jdsacoto@users.noreply.github.com>
2022-02-05 01:56:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,17 @@ import streamlit as st
 # importing matplotlib modules
 from PIL import Image
 
+filename = "Angiop.png"
+img = cv2.imread(filename, 1)
+image = np.array([img])
+
+original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
+st.markdown(original_title, unsafe_allow_html=True)
+st.image(image, channels="BGR")
+
+new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">New image</p>'
+st.markdown(new_title, unsafe_allow_html=True)
+st.image(image, channels="BGR")
 
 
 
