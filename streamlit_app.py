a82ac80e87015e99d93e6e9d23008f14ebc93539
jdsacoto <46428518+jdsacoto@users.noreply.github.com>
2022-02-05 01:31:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,15 @@ print("Hello world")
 #pip install streamlit
 #streamlit hello
 import streamlit as st
+# importing matplotlib modules
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+  
+# Read Images
+img = mpimg.imread('g4g.png')
+  
+# Output Images
+plt.imshow(img)
 
 """
 # ANGIOP.AI
